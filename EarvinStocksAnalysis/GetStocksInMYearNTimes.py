e49fc80019b2e52ded3dcc4263555e64538710cd
Earvin Lin(SG3) <lin.earvin@gmail.com(SG3)>
2022-10-03 23:02:05 +0800

    20221003-2301 Write stocks analysis programs.

--- EarvinStocksAnalysis/GetStocksInMYearNTimes.py
+++ EarvinStocksAnalysis/GetStocksInMYearNTimes.py
@@ -23,11 +23,9 @@ db   = 'stocksdb'
 # 相對路徑：預設為Windows Platform
 DATA_PATH = "DATA\\MYNT\\"
 
-# 打算比對的次數
-DEF_COMP_TIME = 8
-
-input_file = "STOCKS_LIST_test.txt"
-output_file = "M_YEAR_N_TIMES_STOCKS_LIST.txt"
+#inputFile = "STOCKS_LIST_test.txt"
+inputFile = "STOCKS_LIST.txt"
+outputFile = ""
 readCnt = 0
 
 cnx = mysql.connector.connect(user=user, password=pwd, host=host, database=db)
@@ -44,14 +42,16 @@ try :
 	theSQLCmd = "select stock_no, dividend_year, stock_price_year, " + \
 				"year_high_price, year_low_price from stocks_dividend " + \
 				"where year_high_price is not null and " + \
+				"dividend_year > 2011 and " + \
 				"stock_no = %s order by stock_price_year, dividend_year "
 
-	print(input_file)
-	twStocksList = open(input_file, 'r')	# 預設以系統編碼開啟
+	print(inputFile)
+	twStocksList = open(inputFile, 'r')	# 預設以系統編碼開啟
 	stocks = twStocksList.readlines()
-	for stockNo in stocks :
+	for stockNo in stocks :	
 		stockNo = stockNo.replace('\n', '')	# 不知為何檔案會有換行符號
 		print("正在處理", stockNo)
+		outputFile = stockNo + ".csv"
 		theArgs = (stockNo,)
 #       判斷每年盈收成長率是否大於10%(連續3年)
 		cursor.execute(theSQLCmd, theArgs)
@@ -60,37 +60,44 @@ try :
 		if not data :
 			print("No data found!!!")
 		else :
-			print("Get it!!!")
 			df = pd.DataFrame(data, columns=['股票代號','股利發放年度', \
-				'股價年度','年度股價最高價','年度股價最低價'])
+				'股價年度','年度最高價','年度最低價'])
 			df['p0'] = None
 			df['p1'] = None
 			df['p2'] = None
-			print(df)
-			print("df counts: ",len(df))
+#			print(df)
+#			print("df counts: ",len(df))
 			counts = len(df)
 			
 			for i in range(counts) :
-				print(i)
-				if df.at[i,'年度股價最低價'] is None :
-					print(df.at[i,'年度股價最低價'])
+#				print(i)
+				if df.at[i,'年度最低價'] is None :
+					print(df.at[i, '年度最低價'])
 					continue
-				lowPrice = df.at[i,'年度股價最低價']
-				highPrice = df.at[i,'年度股價最高價']
+				lowPrice = df.at[i,'年度最低價']
+				highPrice = df.at[i,'年度最高價']
 				diff00 = (highPrice - lowPrice) / lowPrice
 				df.iloc[i,5] = "{:.2f}".format(diff00)
-#				次年				
+#				次年
 				if i < (counts - 1) :
-					highPrice1st = df.at[i + 1,'年度股價最高價']
+					highPrice1st = df.at[i + 1,'年度最高價']
 					diff01 = (highPrice1st - lowPrice) / lowPrice
 					df.iloc[i,6] = "{:.2f}".format(diff01)
 #				2年
 				if i < (counts - 2) :
-					highPrice2nd = df.at[i + 2,'年度股價最高價']
+					highPrice2nd = df.at[i + 2,'年度最高價']
 					diff02 = (highPrice2nd - lowPrice) / lowPrice
 					df.iloc[i,7] = "{:.2f}".format(diff02)
 			print(df)
-			df.to_csv("test.csv", encoding='cp950')
+			
+#			df.to_csv(DATA_PATH + outputFile, encoding='cp950')
+			num0 = df.p0.dropna()
+			num1 = df.p1.dropna()
+			num2 = df.p2.dropna()
+			print(num0.max(), ", ", num1.max(), ", ", num2.max())
+			if float(num0.max()) >= 10 or float(num1.max()) >= 10 or \
+				float(num2.max()) >= 10 :
+				df.to_csv(DATA_PATH + outputFile, encoding='cp950')
 
 		readCnt += 1
 
