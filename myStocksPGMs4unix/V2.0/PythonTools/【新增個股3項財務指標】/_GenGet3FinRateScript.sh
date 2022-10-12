2e402999d2e92a80d430201900885468642e3f03
earvinlin.02(imac) <earvinlin.02@gmail.com>
2022-10-12 15:01:28 +0800

    20221012-1501 Get 3 financial indexes.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
@@ -1,99 +1,16 @@
-python3 01_getStocksDailyRatiosData.py 20070101
-python3 01_formatStocksDailyRatiosData.py 20070101
+python3 01_getStocksDailyRatiosData.py 20070517
+python3 01_formatStocksDailyRatiosData.py 20070517
 sleep 10
 # wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070201
-python3 01_formatStocksDailyRatiosData.py 20070201
+python3 01_getStocksDailyRatiosData.py 20070518
+python3 01_formatStocksDailyRatiosData.py 20070518
 sleep 10
 # wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070301
-python3 01_formatStocksDailyRatiosData.py 20070301
+python3 01_getStocksDailyRatiosData.py 20070905
+python3 01_formatStocksDailyRatiosData.py 20070905
 sleep 10
 # wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070401
-python3 01_formatStocksDailyRatiosData.py 20070401
+python3 01_getStocksDailyRatiosData.py 20070906
+python3 01_formatStocksDailyRatiosData.py 20070906
 sleep 10
 # wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070501
-python3 01_formatStocksDailyRatiosData.py 20070501
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070601
-python3 01_formatStocksDailyRatiosData.py 20070601
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070701
-python3 01_formatStocksDailyRatiosData.py 20070701
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070801
-python3 01_formatStocksDailyRatiosData.py 20070801
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20070901
-python3 01_formatStocksDailyRatiosData.py 20070901
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20071001
-python3 01_formatStocksDailyRatiosData.py 20071001
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20071101
-python3 01_formatStocksDailyRatiosData.py 20071101
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20071201
-python3 01_formatStocksDailyRatiosData.py 20071201
-sleep 10
-# wait 10 sec.
-
-python3 01_getStocksDailyRatiosData.py 20080101
-python3 01_formatStocksDailyRatiosData.py 20080101
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080201
-python3 01_formatStocksDailyRatiosData.py 20080201
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080301
-python3 01_formatStocksDailyRatiosData.py 20080301
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080401
-python3 01_formatStocksDailyRatiosData.py 20080401
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080501
-python3 01_formatStocksDailyRatiosData.py 20080501
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080601
-python3 01_formatStocksDailyRatiosData.py 20080601
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080701
-python3 01_formatStocksDailyRatiosData.py 20080701
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080801
-python3 01_formatStocksDailyRatiosData.py 20080801
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20080901
-python3 01_formatStocksDailyRatiosData.py 20080901
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20081001
-python3 01_formatStocksDailyRatiosData.py 20081001
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20081101
-python3 01_formatStocksDailyRatiosData.py 20081101
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20081201
-python3 01_formatStocksDailyRatiosData.py 20081201
-sleep 10
-# wait 10 sec.
-
-
