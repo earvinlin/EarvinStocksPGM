ac656473a5f916a75a775eb5cef464fbf4e81971
earvinlin.02(imac) <earvinlin.02@gmail.com>
2022-10-12 14:53:44 +0800

    20221012-1453 Get 3 financial indexes.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
@@ -47,8 +47,53 @@ python3 01_formatStocksDailyRatiosData.py 20071201
 sleep 10
 # wait 10 sec.
 
-python3 01_getStocksDailyRatiosData.py 20061201
-python3 01_formatStocksDailyRatiosData.py 20061201
+python3 01_getStocksDailyRatiosData.py 20080101
+python3 01_formatStocksDailyRatiosData.py 20080101
 sleep 10
 # wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080201
+python3 01_formatStocksDailyRatiosData.py 20080201
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080301
+python3 01_formatStocksDailyRatiosData.py 20080301
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080401
+python3 01_formatStocksDailyRatiosData.py 20080401
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080501
+python3 01_formatStocksDailyRatiosData.py 20080501
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080601
+python3 01_formatStocksDailyRatiosData.py 20080601
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080701
+python3 01_formatStocksDailyRatiosData.py 20080701
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080801
+python3 01_formatStocksDailyRatiosData.py 20080801
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20080901
+python3 01_formatStocksDailyRatiosData.py 20080901
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20081001
+python3 01_formatStocksDailyRatiosData.py 20081001
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20081101
+python3 01_formatStocksDailyRatiosData.py 20081101
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20081201
+python3 01_formatStocksDailyRatiosData.py 20081201
+sleep 10
+# wait 10 sec.
+
 
