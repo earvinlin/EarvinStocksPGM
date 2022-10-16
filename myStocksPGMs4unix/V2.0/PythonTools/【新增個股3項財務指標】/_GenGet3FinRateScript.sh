85cbb98ba45887832fd34e18fda9b2c84ecacafb
earvinlin.02(imac) <earvinlin.02@gmail.com>
2022-10-16 09:49:10 +0800

    20221016-0948 補抓未成功的檔案

--- myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
@@ -1,4 +1,39 @@
-python3 01_getStocksDailyRatiosData.py 20210605
-python3 01_formatStocksDailyRatiosData.py 20210605
+python3 01_getStocksDailyRatiosData.py 20051101
+python3 01_formatStocksDailyRatiosData.py 20051101
 sleep 10
 # wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20060316
+python3 01_formatStocksDailyRatiosData.py 20060316
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20061208
+python3 01_formatStocksDailyRatiosData.py 20061208
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20070911
+python3 01_formatStocksDailyRatiosData.py 20070911
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20071005
+python3 01_formatStocksDailyRatiosData.py 20071005
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20071126
+python3 01_formatStocksDailyRatiosData.py 20071126
+sleep 10
+# wait 10 sec.
+ python3 01_getStocksDailyRatiosData.py 20140401
+python3 01_formatStocksDailyRatiosData.py 20140401
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20140402
+python3 01_formatStocksDailyRatiosData.py 20140402
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20140403
+python3 01_formatStocksDailyRatiosData.py 20140403
+sleep 10
+# wait 10 sec.
+python3 01_getStocksDailyRatiosData.py 20150312
+python3 01_formatStocksDailyRatiosData.py 20150312
+sleep 10
