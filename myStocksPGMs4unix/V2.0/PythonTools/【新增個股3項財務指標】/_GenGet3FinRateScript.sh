477c558ddf675ae9d67815c2e9811253b998129b
earvinlin.02(imac) <earvinlin.02@gmail.com>
2022-10-21 12:32:02 +0800

    20221021-1231 Modified files.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_GenGet3FinRateScript.sh
@@ -1,17 +1,8 @@
-python3 01_getStocksDailyRatiosData.py 20111027
-python3 01_formatStocksDailyRatiosData.py 20111027
+python3 01_getStocksDailyRatiosData.py 20141231
+python3 01_formatStocksDailyRatiosData.py 20141231
 sleep 10
 # wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20180916
-python3 01_formatStocksDailyRatiosData.py 20180916
+python3 01_getStocksDailyRatiosData.py 20150414
+python3 01_formatStocksDailyRatiosData.py 20150414
 sleep 10
 # wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20190707
-python3 01_formatStocksDailyRatiosData.py 20190707
-sleep 10
-# wait 10 sec.
-python3 01_getStocksDailyRatiosData.py 20190825
-python3 01_formatStocksDailyRatiosData.py 20190825
-sleep 10
-# wait 10 sec.
-
