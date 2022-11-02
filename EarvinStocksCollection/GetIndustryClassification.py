8b1891db7378410380369d9167e952af8a7b1a05
Earvin Lin(SG3) <lin.earvin@gmail.com(SG3)>
2022-11-02 15:47:16 +0800

    20221102-1546 GetIndustryClassification.py coding.

--- EarvinStocksCollection/GetIndustryClassification.py
+++ EarvinStocksCollection/GetIndustryClassification.py
@@ -1,5 +1,6 @@
 """
 
+python GetIndustryClassification.py IndustryClassification\ ic20221102.txt
 """
 
 import re
@@ -11,14 +12,21 @@ import requests
 #import urllib.request
 from bs4 import BeautifulSoup
 
-"""
-if len(sys.argv) < 2 :
-    print("You need input two parameter(fmt : theDate(yyyymmdd))")
-    print("syntax : C:\python GetIndustryClassification.py 20170401 ")
-    sys.exit()
-"""
 
-HYPER_LINK = "https://sjmain.esunsec.com.tw"
+if len(sys.argv) < 3 :
+    print("You need input two parameter(fmt : directory filename)")
+    print("syntax : C:\python GetIndustryClassification.py IndustryClassification\\ ic20221102.txt ")
+    sys.exit()
+
+
+# for windows now (2022.11.02)
+#saveFileDir = "IndustryClassification\\"
+#fileName = "test.txt"
+saveFileDir = sys.argv[1]
+fileName = sys.argv[2]
+outfile = open(saveFileDir + fileName, 'w')
+
+#HYPER_LINK = "https://sjmain.esunsec.com.tw"
 ssl._create_default_https_context = ssl._create_unverified_context
 #
 # 玉山證券：產業分析 -> 類股行情表
@@ -42,9 +50,15 @@ table = soup.find(lambda tag: tag.name=='table' and \
     tag.has_key('id') and tag['id']=="oMainTable")
 
 
+
+
+HYPER_LINK = "https://sjmain.esunsec.com.tw"
 rowCount = 0
 serialCount = 0
 blnFindTagA = False
+classType = ""
+bln1stRecord = True
+
 for sibling in table.tr.next_siblings:
     print('處理筆數= ', str(rowCount))
     
@@ -53,20 +67,38 @@ for sibling in table.tr.next_siblings:
     print(len(tds), str(blnFindTagA), str(serialCount))
     for theTD in theTR.findAll(lambda tag: tag.name=='td') :
         for link in theTD.findAll('a') :
-            print(str(rowCount), str(serialCount), link.get('href'), link.text)
+            if bln1stRecord == True :
+                classType = "M"
+                bln1stRecord = False
+            else :
+                classType = "S"
+            theAddr = HYPER_LINK + link.get('href')
+            classCode = theAddr[(theAddr.find('a=')+2):]
+#           產業分類(M大項_S子項)、產業分類代碼、網址、產業分類名稱
+#            print(str(rowCount), str(serialCount), classType, classCode, theAddr, link.text)
+            theInsertCmd = "insert into industry_classification (groupid, classificationType, classificationCode, " + \
+                "classificationName, hrefAddr) values (" + str(serialCount) + ", '" + \
+                classType + "', '" + classCode + "', '" + link.text + "', '" + theAddr + "');"
+#            print(theInsertCmd)
+            outfile.write(theInsertCmd + "\n")
             blnFindTagA = True
 
-    print('-----', str(blnFindTagA))
+    print('-----')
     if blnFindTagA == True :
         serialCount += 1
         blnFindTagA = False
-        print("In loop: ", str(blnFindTagA), str(serialCount))
+        bln1stRecord = True
         
     rowCount += 1
+    
 print("END!!! ", str(rowCount))
+outfile.close()
 
 
 
+
+
+"""
 try :
 #   下面這行有bug，會因為平台不同而有錯誤發生(要判斷執行程式的平台)    
     saveFileDir = "IndustryClassification\\"
@@ -79,7 +111,7 @@ try :
 except IOError as err :
     print('Fie error : ' + str(err))
 
-"""
+
 
 rawtext=urlopen('http://www.ccnu.edu.cn',timeout=15).read();
 print(rawtext)
