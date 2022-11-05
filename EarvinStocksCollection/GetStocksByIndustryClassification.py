78bbaeed232f2c38cbae49393db8815afe2f5dbc
Earvin Lin <earvin@VMDebianVIVO.idv>
2022-11-05 17:32:51 +0800

    20221105-1732 GetStocksByIndustryClassification.py coding.

--- EarvinStocksCollection/GetStocksByIndustryClassification.py
+++ EarvinStocksCollection/GetStocksByIndustryClassification.py
@@ -16,10 +16,8 @@ from bs4 import BeautifulSoup
 
 if len(sys.argv) < 3 :
     print("You need input two parameter(fmt : directory filename)")
-    print("syntax : C:\python GetIndustryClassification.py IndustryClassification\\ ic20221102.txt ")
+    print("syntax : C:\python GetStocksByIndustryClassification.py IndustryClassification\\ ic20221102.txt ")
     sys.exit()
-
-
 
 theSaveFileDir = sys.argv[1]
 theInputFile = sys.argv[2]
@@ -27,91 +25,61 @@ theOutputFile = ""
 insertCnt = 0
 errorCnt = 0
 
-
 # for windows now (2022.11.02)
 #saveFileDir = "IndustryClassification\\"
 #fileName = "test.txt"
-saveFileDir = sys.argv[1]
-fileName = sys.argv[2]
-theInputFile = open(saveFileDir + fileName, 'w')
+theSaveFileDir = sys.argv[1]
+theInputFileName = sys.argv[2]
 
 # Step01
 # 先組出要抓檔的分類、來源網址 (Data from file: IC_StocksAddressList.txt)
 #
+#with open(theSaveFileDir + theInputFileName, 'r', encoding = 'CP950') as infile :
+with open(theInputFileName, 'r', encoding = 'utf-8') as theInputFile :
+    for eachLine in theInputFile :
+        theList = eachLine.split(",") 
+        print(theList)
+        theOutputFileName = theSaveFileDir + theList[1] + ".txt"
+        theHyperlink = theList[2].replace("\n", "")
+        print(theOutputFileName)
+#        theOutputFile = open(prnData, 'w')
 
+        print('處理網頁 : ' + theList[1])
+        theOutputFile = open(theOutputFileName, 'w')
+        ssl._create_default_https_context = ssl._create_unverified_context
+        url = "https://sjmain.esunsec.com.tw/z/zh/zha/zha.djhtm"
 
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+        headers = {'User-Agent': user_agent}
 
-# Step02
-# 依序抓網頁資料並
-#
+        resp = requests.get(theHyperlink, headers = headers)
+        resp.encoding = 'cp950'
 
-#HYPER_LINK = "https://sjmain.esunsec.com.tw"
-ssl._create_default_https_context = ssl._create_unverified_context
-#
-# 玉山證券：產業分析 -> 類股行情表
-# https://www.esunsec.com.tw/tw-market/z/ze/zeg/zeg_23.djhtm
-#
-# Source : 玉山證券，資料嵌在iframe
-# iframe : https://sjmain.esunsec.com.tw/z/zh/zha/zha.djhtm
-url = "https://sjmain.esunsec.com.tw/z/zh/zha/zha.djhtm"
+        soup = BeautifulSoup(resp.text, 'lxml')
+        theTable = soup.find(lambda tag: tag.name=='table' and tag.has_key('id') and tag['id']=="oMainTable")
+#        print("script: ", theTable.td.script.string)
+#        print(soup)
+        for sibling in theTable.tr.next_siblings:
+            theTR = BeautifulSoup(repr(sibling), 'lxml')
+            tds = theTR.findAll(lambda tag: tag.name=='td')
+            for theTD in theTR.findAll(lambda tag: tag.name=='td') :
+                print(theTD)
+                theOutputFile.write(str(theTD))
+                theOutputFile.write('\n')
 
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-headers = {'User-Agent': user_agent}
-
-resp = requests.get(url, headers=headers)
-resp.encoding = 'cp950'
-
-#soup = BeautifulSoup(resp.text, 'html.parser')
-soup = BeautifulSoup(resp.text, 'lxml')
-print("title: ", soup.title.text)
-
-table = soup.find(lambda tag: tag.name=='table' and \
-    tag.has_key('id') and tag['id']=="oMainTable")
-
-
-
-
-HYPER_LINK = "https://sjmain.esunsec.com.tw"
-rowCount = 0
-serialCount = 0
-blnFindTagA = False
-classType = ""
-bln1stRecord = True
-
-for sibling in table.tr.next_siblings:
-    print('處理筆數= ', str(rowCount))
-    
-    theTR = BeautifulSoup(repr(sibling), 'lxml')
-    tds = theTR.findAll(lambda tag: tag.name=='td')
-    print(len(tds), str(blnFindTagA), str(serialCount))
-    for theTD in theTR.findAll(lambda tag: tag.name=='td') :
-        for link in theTD.findAll('a') :
-            if bln1stRecord == True :
-                classType = "M"
-                bln1stRecord = False
-            else :
-                classType = "S"
-            theAddr = HYPER_LINK + link.get('href')
-            classCode = theAddr[(theAddr.find('a=')+2):]
-#           產業分類(M大項_S子項)、產業分類代碼、網址、產業分類名稱
-#            print(str(rowCount), str(serialCount), classType, classCode, theAddr, link.text)
-            theInsertCmd = "insert into industry_classification (groupid, classificationType, classificationCode, " + \
-                "classificationName, hrefAddr) values (" + str(serialCount) + ", '" + \
-                classType + "', '" + classCode + "', '" + link.text + "', '" + theAddr + "');"
-#            print(theInsertCmd)
-            outfile.write(theInsertCmd + "\n")
-            blnFindTagA = True
-
-    print('-----')
-    if blnFindTagA == True :
-        serialCount += 1
-        blnFindTagA = False
-        bln1stRecord = True
         
-    rowCount += 1
-    
-print("END!!! ", str(rowCount))
-outfile.close()
+        theOutputFile.close()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
