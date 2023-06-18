4b4e076fc3cef56d548b23e7367c248e65d83452
Earvin Lin(ASUSWinPC) <lin.earvin@gmail.com(ASUSWinPC)>
2023-06-18 17:04:01 +0800

    202306181703 Start write stocks analysis programs.

---
+++ EarvinStocksAnalysis/modules/indexes/IndexMAP.py
@@ -0,0 +1,13 @@
+import os
+
+"""
+技術指標 MAP
+input :
+    stocksData : 股票資料
+    indexDay   : 指標天數
+"""
+#def indexMAP(stocksData, indexDay) :
+#    print(x)
+
+def testMAP(x) :
+    print(x)
---
+++ EarvinStocksAnalysis/testModules.py
@@ -0,0 +1,6 @@
+import os
+#from modules.indexes import ind
+#import modules.indexes.indexMAP as ind
+from modules.indexes import IndexMAP as ind
+
+ind.testMAP("hello modules!")
