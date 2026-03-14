f91440b269158d98f770cc083bc7b3c972f44497
earvinlin.02(mac14pronb) <earvinlin.02@gmail.com>
2026-03-14 11:04:17 +0800

    20260314-1104 add files.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
@@ -1,27 +1,15 @@
 
 #!/bin/bash
 
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260211
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260223
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260224
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260225
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260226
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260309
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260310
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260311
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260312
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260313
 
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260302
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260303
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260304
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260305
-# Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260306
-
-# Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260211
-# Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260223
-# Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260224
-# Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260225
-# Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260226
-
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260302
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260303
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260304
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260305
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260306
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260309
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260310
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260311
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260312
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260313
 
--- myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__MS_ProcessTaiwanDataTsecMargin.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__MS_ProcessTaiwanDataTsecMargin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python MS_getTaiwanDataTsecMarginData_v2.py 20260302 20260306 MS
-python MS_formatTaiwanDataTsecMarginData_v2.py 20260302 20260306 MS
-python MS_insertTaiwanDataTsecMarginToMySQLDB_v2.py 20260302 20260306
+python MS_getTaiwanDataTsecMarginData_v2.py 20260309 20260313 MS
+python MS_formatTaiwanDataTsecMarginData_v2.py 20260309 20260313 MS
+python MS_insertTaiwanDataTsecMarginToMySQLDB_v2.py 20260309 20260313
