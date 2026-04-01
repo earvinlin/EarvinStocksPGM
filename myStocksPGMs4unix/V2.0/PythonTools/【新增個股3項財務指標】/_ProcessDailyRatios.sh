0d70245db70770f8d3a2c78bcd06fa39d75da52c
lin.earvin(hpenvynb) <lin.earvin(hpenvbnb)>
2026-04-01 14:54:48 +0800

    20260401-1454 add files.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_ProcessDailyRatios.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_ProcessDailyRatios.sh
@@ -1,7 +1,3 @@
 python3 01_getStocksDailyRatiosData_v2.py 20260323 20260327
 python3 02_formatStocksDailyRatiosData_v2.py 20260323 20260327
 python3 03_insertDailyRatiosToMySQLDB_v2.py 20260323 20260327
-<<<<<<< HEAD
-
-=======
->>>>>>> 8766cc4f34a9fed661a2a0ba896e527aaca19f43
--- myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260323
@@ -6,20 +5,10 @@ Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260324
 Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260325
 Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260326
 Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260327
-<<<<<<< HEAD
 
 Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260323
 Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260324
 Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260325
 Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260326
 Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260327
-
-=======
-
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260323
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260324
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260325
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260326
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260327
->>>>>>> 8766cc4f34a9fed661a2a0ba896e527aaca19f43
 
