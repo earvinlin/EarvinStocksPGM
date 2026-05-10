730e7e46b822810bd542990c5017c7721f97ab98
earvinlin.02(imac) <earvinlin.02@gmail.com(imac)>
2026-05-11 07:26:33 +0800

    20260511-0726 add files.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_ProcessDailyRatios.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_ProcessDailyRatios.sh
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-# python3 01_getStocksDailyRatiosData_v2.py 20260413 20260417
-# python3 02_formatStocksDailyRatiosData_v2.py 20260413 20260417
-=======
-python3 01_getStocksDailyRatiosData_v2.py 20260413 20260417
-python3 02_formatStocksDailyRatiosData_v2.py 20260413 20260417
->>>>>>> 29e77456f8b6a7ece494125d6bfdd9b0409b0d7a
-python3 03_insertDailyRatiosToMySQLDB_v2.py 20260413 20260417
+#!/bin/bash
+
+python3 01_getStocksDailyRatiosData_v2.py 20260420 20260424
+python3 02_formatStocksDailyRatiosData_v2.py 20260420 20260424
+python3 03_insertDailyRatiosToMySQLDB_v2.py 20260420 20260424
--- myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260413
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260414
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260415
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260416
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260417
-<<<<<<< HEAD
-=======
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260420
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260421
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260422
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260423
+Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260424
 
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260413
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260414
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260415
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260416
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260417
->>>>>>> 29e77456f8b6a7ece494125d6bfdd9b0409b0d7a
-
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260413
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260414
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260415
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260416
-Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260417
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260420
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260421
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260422
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260423
+Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260424
