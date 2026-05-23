31b3c928c55dce29ce46eb2245aee7602274391e
lin.earvin(hpenvynb) <lin.earvin(hpenvbnb)>
2026-05-23 08:47:43 +0800

    20260523-0847 add files.

--- myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__MS_ProcessTaiwanDataTsecMargin.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__MS_ProcessTaiwanDataTsecMargin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python MS_getTaiwanDataTsecMarginData_v2.py 20260511 20260515 MS
-python MS_formatTaiwanDataTsecMarginData_v2.py 20260511 20260515 MS
-python MS_insertTaiwanDataTsecMarginToMySQLDB_v2.py 20260511 20260515
+python MS_getTaiwanDataTsecMarginData_v2.py 20260518 20260522 MS
+python MS_formatTaiwanDataTsecMarginData_v2.py 20260518 20260522 MS
+python MS_insertTaiwanDataTsecMarginToMySQLDB_v2.py 20260518 20260522
