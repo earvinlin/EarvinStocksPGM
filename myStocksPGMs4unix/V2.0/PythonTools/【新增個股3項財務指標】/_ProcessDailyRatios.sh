95f3e0dd0e7f55f77af422a14431754150ddc3a1
earvinlin.02(mac14pronb) <earvinlin.02@gmail.com>
2026-06-28 08:46:19 +0800

    20260628-0846 add files.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_ProcessDailyRatios.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增個股3項財務指標】/_ProcessDailyRatios.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 01_getStocksDailyRatiosData_v2.py 20260608 20260612
-python3 02_formatStocksDailyRatiosData_v2.py 20260608 20260612
-python3 03_insertDailyRatiosToMySQLDB_v2.py 20260608 20260612
+python3 01_getStocksDailyRatiosData_v2.py 20260615 20260618
+python3 02_formatStocksDailyRatiosData_v2.py 20260615 20260618
+python3 03_insertDailyRatiosToMySQLDB_v2.py 20260615 20260618
