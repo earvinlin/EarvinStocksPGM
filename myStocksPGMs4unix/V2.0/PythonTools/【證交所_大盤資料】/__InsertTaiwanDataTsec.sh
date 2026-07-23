7ffd0ba1c9b6b90815f81c8447df94f319fd7a5b
earvinlin.02(imac) <earvinlin.02@gmail.com(imac)>
2026-07-23 16:18:05 +0800

    20260723-1617 add files.

--- myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__InsertTaiwanDataTsec.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__InsertTaiwanDataTsec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Python3 02_insertTaiwanDataTsecToMySQLDB.py 20260501
+#Python3 02_insertTaiwanDataTsecToMySQLDB.py 20260601
 
-Python3 02_insertTaiwanDataTsecToMySQLDB.py 20260601
 Python3 02_insertTaiwanDataTsecToMySQLDB.py 20260701
--- myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__InsertTaiwanDataTsecVolume.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__InsertTaiwanDataTsecVolume.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Python3 04_insertTaiwanDataTsecVolumeToMySQLDB.py 20260501
+#Python3 04_insertTaiwanDataTsecVolumeToMySQLDB.py 20260601
 
-Python3 04_insertTaiwanDataTsecVolumeToMySQLDB.py 20260601
 Python3 04_insertTaiwanDataTsecVolumeToMySQLDB.py 20260701
