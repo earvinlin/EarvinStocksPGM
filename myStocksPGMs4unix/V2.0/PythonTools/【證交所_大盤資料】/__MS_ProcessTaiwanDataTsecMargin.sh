a711a785adc1c0dbf09c3cc7c5ef5a544b17b0cd
earvinlin.02(mac14pronb) <earvinlin.02@gmail.com>
2026-07-25 09:53:41 +0800

    20260725-0953 add file.

--- myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__MS_ProcessTaiwanDataTsecMargin.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【證交所_大盤資料】/__MS_ProcessTaiwanDataTsecMargin.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-python MS_getTaiwanDataTsecMarginData_v2.py 20260713 20260717 MS
-python MS_formatTaiwanDataTsecMarginData_v2.py 20260713 20260717 MS
-python MS_insertTaiwanDataTsecMarginToMySQLDB_v2.py 20260713 20260717
-=======
 python MS_getTaiwanDataTsecMarginData_v2.py 20260720 20260724 MS
 python MS_formatTaiwanDataTsecMarginData_v2.py 20260720 20260724 MS
 python MS_insertTaiwanDataTsecMarginToMySQLDB_v2.py 20260720 20260724
->>>>>>> 7ffd0ba1c9b6b90815f81c8447df94f319fd7a5b
