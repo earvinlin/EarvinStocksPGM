4b14f7cb26182e82051d2c9054d5737de8e204bf
earvin(vivopc) <lin.earvin@gmail.com(vivopc)>
2026-08-21 11:37:15 +0800

    20260821-1137 add files.

--- EarvinStocksPGM_CS/EarvinSTocksPGM_CS/EarvinSTocksPGM_CS/obj/Debug/net8.0-windows/EarvinSTocksPGM_CS.AssemblyInfo.cs
+++ EarvinStocksPGM_CS/EarvinSTocksPGM_CS/EarvinSTocksPGM_CS/obj/Debug/net8.0-windows/EarvinSTocksPGM_CS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EarvinSTocksPGM_CS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79d9d99f64d967c1a8edcd72eb9a95e0e4aa364b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5d886799d4f7e7c7648798f4531c575c67b467")]
 [assembly: System.Reflection.AssemblyProductAttribute("EarvinSTocksPGM_CS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EarvinSTocksPGM_CS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
