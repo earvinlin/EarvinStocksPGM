d3b17f7e46324373296fd6e700af4f9ebbe8f682
earvin(vivopc) <lin.earvin@gmail.com(vivopc)>
2026-08-21 09:06:00 +0800

    20260821-0905 改版唸書時寫的股票程式。

---
+++ EarvinStocksPGM_CS/EarvinSTocksPGM_CS/EarvinSTocksPGM_CS/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EarvinSTocksPGM_CS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
