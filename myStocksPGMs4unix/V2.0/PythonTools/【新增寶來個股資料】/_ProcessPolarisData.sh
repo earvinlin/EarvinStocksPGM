3eee4f70c5a0c0da0d0498102569ff275e980e2c
earvinlin.02(mac14pronb) <earvinlin.02@gmail.com>
2026-08-21 21:30:16 +0800

    20260821-2130 add files.

--- myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
+++ myStocksPGMs4unix/V2.0/PythonTools/【新增寶來個股資料】/_ProcessPolarisData.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 #Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260810
 #Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260811
 #Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260812
 #Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260813
 #Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260814
-=======
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260810
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260811
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260812
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260813
-Python3 FormatTaiwanDataPolarisWithParams_V1.4.py Close20260814
->>>>>>> b2b7bb5b6f6fd2b9862d7a109e0d17b3a01c6a62
 
 Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260810
 Python3 InsertTaiwanDataPolarisWithParams_V1.4.py Close20260811
